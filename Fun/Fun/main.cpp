0491a53508859780d5132073bc1d9c826625496e
lyndseyh <lyndseyh@umich.edu>
2014-11-20 15:48:05 -0500

    Add Repository

---
+++ Fun/Fun/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Fun
+//
+//  Created by Lyndsey on 11/20/14.
+//  Copyright (c) 2014 Lyndsey Housman. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
